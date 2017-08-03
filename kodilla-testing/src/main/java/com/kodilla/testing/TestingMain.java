b858e4465c2a42c0ea271ed16649daa2b2f28b8d
oklincewicz <olga.klincewicz@gmail.com>
2017-08-03 19:12:23 +0200

    First Test Module 6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,29 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        SimpleUser simplyUser = new SimpleUser("theForumUser");
+
+        String result = simplyUser.getUserName();
+
+        if(result.equals("theForumUser")) {
+            System.out.println("Test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+
+        int resultAdd = calculator.add(2, 5);
+        int resultSubtract = calculator.subtract(12, 5);
+
+        if (resultAdd == 7 && resultSubtract == 7) {
+            System.out.println("Test OK");
+        } else {
+            System.out.println("Error!");
+        }
     }
 }
