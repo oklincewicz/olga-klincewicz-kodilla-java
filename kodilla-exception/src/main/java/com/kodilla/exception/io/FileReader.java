225c35e7569d4466cfaef0494119f19a4a010195
oklincewicz <olga.klincewicz@gmail.com>
2017-08-18 20:45:34 +0300

    Moduł 8 - Wyjątki

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -9,7 +9,7 @@ import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 public class FileReader {
-    public void readFile() {
+    public void readFile() throws FileReaderException {
 
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("file/names.txt").getFile());
@@ -19,7 +19,7 @@ public class FileReader {
             fileLines.forEach(System.out::println);
 
         } catch (IOException e) {
-            System.out.println("Sth go wrong" + e);
+            throw new FileReaderException();
         } finally {
             System.out.println("I'm gonna be here.. always");
         }
---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/SecondChallenge.java
@@ -0,0 +1,24 @@
+package com.kodilla.exception.test;
+
+public class SecondChallenge {
+    public String probablyIWillThrowException(double x, double y) throws Exception {
+        if(x >= 2 || x < 1 || y == 1.5) {
+            throw new Exception();
+        }
+        return "Done!";
+    }
+
+    public static void main(String[] args) {
+
+        SecondChallenge secondChallenge = new SecondChallenge();
+
+        try {
+            secondChallenge.probablyIWillThrowException(2, 1.5);
+        } catch (Exception e) {
+            System.out.println("You have problem with Exception " + e);
+        } finally {
+            System.out.println("I hope that helped you?");
+        }
+
+    }
+}
