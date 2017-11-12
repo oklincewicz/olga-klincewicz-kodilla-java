1b58012552eb2bb5b50156e97d768444454f8787
oklincewicz <olga.klincewicz@gmail.com>
2017-11-12 14:52:31 +0100

    Moduł 20.3

--- kodilla-patternsPart2/src/main/java/com/kodilla/patterns2/facade/api/OrderFacade.java
+++ kodilla-patternsPart2/src/main/java/com/kodilla/patterns2/facade/api/OrderFacade.java
@@ -5,12 +5,15 @@ import com.sun.org.apache.xpath.internal.operations.Or;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 
 
 @Service
+@EnableAspectJAutoProxy
 public class OrderFacade {
     @Autowired
     private ShopService shopService;
