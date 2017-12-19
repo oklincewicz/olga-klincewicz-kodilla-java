f65f6566496f33d10dfb61051decffc2e6b59b97
oklincewicz <olga.klincewicz@gmail.com>
2017-12-19 21:04:19 +0100

    Moduł 23.3

---
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
@@ -0,0 +1,35 @@
+package com.kodilla.testing2.facebook;
+
+import com.kodilla.testing2.config.WebDriverConfig;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+public class FacebookTestingApp {
+
+    public static final String XPATH_DAY = "//html/body/div/div[4]/div/div/div/div/div/div[2]/div[2]/div/div/div/div/form/div/div[6]/div[2]/span/span/select";
+    public static final String XPATH_MONTH = "//html/body/div/div[4]/div/div/div/div/div/div[2]/div[2]/div/div/div/div/form/div/div[6]/div[2]/span/span/select[2]";
+    public static final String XPATH_YEAR = "//html/body/div/div[4]/div/div/div/div/div/div[2]/div[2]/div/div/div/div/form/div/div[6]/div[2]/span/span/select[3]";
+
+
+    public static void main(String[] args) {
+
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
+        driver.get("https://www.facebook.pl/");
+
+        WebElement selectDay = driver.findElement(By.xpath(XPATH_DAY));
+        Select selectDayCombo = new Select(selectDay);
+        selectDayCombo.selectByValue("7");
+
+        WebElement selectMonth = driver.findElement(By.xpath(XPATH_MONTH));
+        Select selectMonthCombo = new Select(selectMonth);
+        selectMonthCombo.selectByValue("9");
+
+        WebElement selectYear = driver.findElement(By.xpath(XPATH_YEAR));
+        Select selectYearCombo = new Select(selectYear);
+        selectYearCombo.selectByValue("1986");
+
+
+    }
+}
--- kodilla-testing2/src/main/java/com/kodilla/testing2/google/GoogleTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/google/GoogleTestingApp.java
@@ -9,8 +9,8 @@ public class GoogleTestingApp {
     public static final String SEARCHFIELD = "lst-ib";
 
     public  static void main(String[] args) {
-        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.FIREFOX);
-        driver.get("https://www.google.com");
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
+        driver.get("https://www.google.com/");
 
         WebElement searchField = driver.findElement(By.id(SEARCHFIELD));
         searchField.sendKeys("Kodilla");
